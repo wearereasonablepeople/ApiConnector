c4920e1828c17b705410a3408b308fdb73fdcf7d
Oleksii Dykan <alick_bass@mail.ru>
2016-11-01 09:44:58 +0100

    Initial Commit

---
+++ ApiConnector/ApiConnector.h
@@ -0,0 +1,19 @@
+//
+//  ApiConnector.h
+//  ApiConnector
+//
+//  Created by Oleksii on 01/11/2016.
+//  Copyright © 2016 WeAreReasonablePeople. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ApiConnector.
+FOUNDATION_EXPORT double ApiConnectorVersionNumber;
+
+//! Project version string for ApiConnector.
+FOUNDATION_EXPORT const unsigned char ApiConnectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ApiConnector/PublicHeader.h>
+
+
