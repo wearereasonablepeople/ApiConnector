b41b617139e9f00d736ef0bcf50c1cac68716bcf
Oleksii Dykan <alick_bass@mail.ru>
2017-01-23 09:49:49 +0100

    Add Router path type

---
+++ ApiConnector/RoutePath.swift
@@ -0,0 +1,41 @@
+//
+//  RoutePath.swift
+//  ApiConnector
+//
+//  Created by Oleksii on 20/01/2017.
+//  Copyright © 2017 WeAreReasonablePeople. All rights reserved.
+//
+
+import Foundation
+
+public protocol RoutePathComponent {
+    var stringValue: String { get }
+    var pathValue: String { get }
+}
+
+public extension RoutePathComponent {
+    public var pathValue: String {
+        return RoutePath.separator + stringValue
+    }
+}
+
+public struct RoutePath {
+    fileprivate static let separator = "/"
+    fileprivate let path: [RoutePathComponent]
+    
+    public init(_ path: RoutePathComponent...) {
+        self.path = path
+    }
+}
+
+extension RoutePath: RoutePathComponent {
+    public var stringValue: String {
+        return path.map({ $0.stringValue }).joined(separator: RoutePath.separator)
+    }
+}
+
+extension String: RoutePathComponent {
+    public var stringValue: String {
+        return self
+    }
+}
