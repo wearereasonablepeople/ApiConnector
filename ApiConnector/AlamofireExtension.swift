c0863dd465f308976fcb10ccc86fec19c9960471
Oleksii Dykan <alick_bass@mail.ru>
2017-03-22 10:49:53 +0100

    Update Alamofire to conform to new Data Request protocol

--- ApiConnector/AlamofireExtension.swift
+++ ApiConnector/AlamofireExtension.swift
@@ -7,20 +7,40 @@
 //
 
 import Alamofire
+import RxSwift
+
+public enum ApiNetworkError: Error {
+    case noErrorNoData
+}
 
 extension Alamofire.DataRequest: DataRequestType {
-    
-    public static func dataRequest(with request: URLRequest) -> Self {
-        return convertWithRequest(with: request)
-    }
-    
-    static func convertWithRequest<T>(with request: URLRequest) -> T {
-        return Alamofire.request(request) as! T
-    }
-    
-    public func responseData(completionHandler: @escaping (Data?, Error?) -> Void) -> Self {
-        return responseData { response in
-            completionHandler(response.result.value, response.result.error)
-        }
+    public static func requestObservable(with request: URLRequest, _ validation: (DataRequest.Validation)?) -> Observable<Response<Data?>> {
+        return Observable.create({ observer -> Disposable in
+            var request = Alamofire.request(request)
+            
+            if let validation = validation {
+                request = request.validate(validation)
+            } else {
+                request = request.validate()
+            }
+            
+            request.responseData() { responseValue in
+                if let request = responseValue.request, let response = responseValue.response {
+                    observer.onNext(
+                        Response(
+                            request: request,
+                            response: response,
+                            value: responseValue.data
+                        )
+                    )
+                } else {
+                    observer.onError(responseValue.error ?? ApiNetworkError.noErrorNoData)
+                }
+            }
+            
+            return Disposables.create {
+                request.cancel()
+            }
+        })
     }
 }
