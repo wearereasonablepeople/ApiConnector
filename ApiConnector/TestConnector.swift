c9bb1add929090db56b9ac443a7f30f980457c10
Oleksii Dykan <alick_bass@mail.ru>
2017-03-22 10:53:01 +0100

    Update TestConnector to new DataRequest protocol

--- ApiConnector/TestConnector.swift
+++ ApiConnector/TestConnector.swift
@@ -7,35 +7,21 @@
 //
 
 import Alamofire
+import RxSwift
 
 public final class TestConnector<T: ResponseProvider>: DataRequestType {
-    let validation: DataRequest.Validation?
-    let request: URLRequest
-    let completionHandler: ((Data?, Error?) -> Void)?
-    
-    public static func dataRequest(with request: URLRequest) -> TestConnector {
-        return .init(request: request)
+    public static func requestObservable(with request: URLRequest, _ validation: (DataRequest.Validation)?) -> Observable<Response<Data?>> {
+        return Observable
+            .just()
+            .observeOn(SerialDispatchQueueScheduler(qos: .userInitiated))
+            .flatMap { Observable.just(T.response(for: request)) }
+            .map { $0.validate(validation ?? defaultValidation) }
+            .map { try $0.toResponse() }
+            .observeOn(MainScheduler.instance)
     }
     
-    public init(request: URLRequest, validation: DataRequest.Validation? = nil, completionHandler: ((Data?, Error?) -> Void)? = nil) {
-        self.completionHandler = completionHandler
-        self.request = request
-        self.validation = validation
-        startIfPossible()
-    }
-    
-    private func startIfPossible() {
-        guard let completionHandler = completionHandler else { return }
-        DispatchQueue.global().async {
-            let response = T.response(for: self.request).validate(self.validation).completionValue
-            DispatchQueue.main.async {
-                completionHandler(response.0, response.1)
-            }
-        }
-    }
-    
-    public func validate() -> TestConnector {
-        return validate({ request, response, data -> Request.ValidationResult in
+    private static var defaultValidation: DataRequest.Validation {
+        return { request, response, data -> Request.ValidationResult in
             let code = response.statusCode
             switch code {
             case 200...299:
@@ -44,18 +30,6 @@ public final class TestConnector<T: ResponseProvider>: DataRequestType {
                 let error = AFError.responseValidationFailed(reason: .unacceptableStatusCode(code: code))
                 return .failure(error)
             }
-        })
-    }
-    
-    public func validate(_ validation: @escaping DataRequest.Validation) -> TestConnector {
-        return .init(request: request, validation: validation)
-    }
-    
-    public func cancel() {
-        //Does nothing
-    }
-    
-    public func responseData(completionHandler: @escaping (Data?, Error?) -> Void) -> TestConnector {
-        return .init(request: request, validation: validation, completionHandler: completionHandler)
+        }
     }
 }
