5f74fd39d7d8f9a134309c1d24700f6ac19eceae
Mirellys Arteta Davila <mirellys@wearereasonablepeople.com>
2017-03-28 14:55:15 +0200

    set default validation on test connector

--- ApiConnector/TestConnector.swift
+++ ApiConnector/TestConnector.swift
@@ -19,7 +19,7 @@ public final class TestConnector<T: ResponseProvider>: DataRequestType {
             .just()
             .observeOn(SerialDispatchQueueScheduler(qos: .userInitiated))
             .flatMap { T.response(for: request) }
-            .map { try $0.validate(validation ?? Response.defaultValidation) }
+            .map { try $0.validate(Response.defaultValidation) }
             .observeOn(MainScheduler.instance)
     }
 }
