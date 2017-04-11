5762525589e1fb0b09fc242d781d9fbca45a53cf
Mirellys Arteta Davila <mirellys@wearereasonablepeople.com>
2017-04-11 15:44:21 +0200

    Tests for changes on HTTP.Value

--- ApiConnectorTests/HTTPTests.swift
+++ ApiConnectorTests/HTTPTests.swift
@@ -12,24 +12,32 @@ import ApiConnector
 
 class HTTPTests: XCTestCase {
     
-    func testHTTPHashable() {
+    func testHTTKeyPHashable() {
         XCTAssertEqual(HTTP.Header.Key.accept, "Accept")
         XCTAssertNotEqual(HTTP.Header.Key.accept, "Accept-Language")
         XCTAssertEqual(HTTP.Header.Key.accept.hashValue, HTTP.Header.Key("Accept").hashValue)
     }
     
+    func testHTTPValueHashable() {
+        XCTAssertEqual(HTTP.Header.Value.applicationJson, "application/json")
+        XCTAssertNotEqual(HTTP.Header.Value.applicationJson, "application/javascript")
+        XCTAssertEqual(HTTP.Header.Value.applicationJson.hashValue, HTTP.Header.Value("application/json").hashValue)
+    }
+    
     func testHTTPToString() {
-        let headers: HTTP.Headers = [.accept: "application/json" ,
-                                     .acceptLanguage: "en-US",
-                                     .contentType: "charset=utf-8"]
-        let result = [HTTP.Header.Key.accept.rawValue: "application/json" ,
-                      HTTP.Header.Key.acceptLanguage.rawValue: "en-US",
-                      HTTP.Header.Key.contentType.rawValue: "charset=utf-8"]
+        let headers: HTTP.Headers = [.accept: .applicationJson,
+                                     .acceptLanguage: .language_en_EN,
+                                     .contentType: .charset_utf_8]
+        let result = [HTTP.Header.Key.accept.rawValue: HTTP.Header.Value.applicationJson.rawValue,
+                      HTTP.Header.Key.acceptLanguage.rawValue: HTTP.Header.Value.language_en_EN.rawValue,
+                      HTTP.Header.Key.contentType.rawValue: HTTP.Header.Value.charset_utf_8.rawValue]
          XCTAssertEqual(result, HTTP.Header.toStringKeys(headers: headers))
     }
     
     func testHTTPHeadersStringLiteral(){
         XCTAssertEqual(HTTP.Header.Key(unicodeScalarLiteral: "김").rawValue, "김")
         XCTAssertEqual(HTTP.Header.Key(extendedGraphemeClusterLiteral: "\u{00032}\u{00032}").rawValue, "22")
+        XCTAssertEqual(HTTP.Header.Value(unicodeScalarLiteral: "김").rawValue, "김")
+        XCTAssertEqual(HTTP.Header.Value(extendedGraphemeClusterLiteral: "\u{00032}\u{00032}").rawValue, "22")
     }
 }
