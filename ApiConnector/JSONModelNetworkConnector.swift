d7cf5c6d89360cc5d4856888ad02f45eeb801bbb
Mirellys Arteta Davila <mirellys710@gmail.com>
2017-10-04 11:06:48 +0200

    Refactor code to implement codable protocol

--- ApiConnector/JSONModelNetworkConnector.swift
+++ ApiConnector/JSONModelNetworkConnector.swift
@@ -7,16 +7,10 @@
 //
 
 import RxSwift
-import SwiftyJSON
-import SwiftyJSONModel
 
 public extension ObservableType where E == Response {
-    public func toModel<T: JSONInitializable>() -> Observable<T> {
-        return map { try T(json: JSON(data: $0.data)) }
-    }
-    
-    public func toModel<T: JSONInitializable>() -> Observable<[T]> {
-        return map { try JSON(data: $0.data).arrayValue().map({ try T(json: $0) }) }
+    public func toModel<T: Decodable>() -> Observable<T> {
+        return map { try JSONDecoder().decode(T.self, from: $0.data) }
     }
     
     public func toVoid() -> Observable<Void> {
@@ -29,15 +23,13 @@ public extension ObservableType where E == Response {
 }
 
 public extension ApiConnectionType {
-    public func requestObservable(method: HTTP.Method = .get, with model: JSONRepresentable?, at endpoint: RouterType.Route, headers: HTTP.Headers? = nil, _ validation: Response.Validation? = nil) -> Observable<Response> {
-        return requestObservable(method: method, with: model.flatMap({ try? $0.jsonValue.rawData() }), at: endpoint, headers: headers, validation)
-    }
-    
-    public func requestObservable<T: JSONInitializable>(method: HTTP.Method = .get, with model: JSONRepresentable? = nil, at endpoint: RouterType.Route, headers: HTTP.Headers? = nil, _ validation: Response.Validation? = nil) -> Observable<T> {
-        return requestObservable(method: method, with: model, at: endpoint, headers: headers, validation).toModel()
-    }
-    
-    public func requestObservable<T: JSONInitializable>(method: HTTP.Method = .get, with model: JSONRepresentable? = nil, at endpoint: RouterType.Route, headers: HTTP.Headers? = nil, _ validation: Response.Validation? = nil) -> Observable<[T]> {
-        return requestObservable(method: method, with: model, at: endpoint, headers: headers, validation).toModel()
+    public func requestObservable<T: Encodable>(method: HTTP.Method = .get, with model: T?, at endpoint: RouterType.Route, headers: HTTP.Headers? = nil, _ validation: Response.Validation? = nil) -> Observable<Response> {
+        let data: Data?
+        do {
+            data = try model.map({ try JSONEncoder().encode($0) })
+        } catch let error {
+            return .error(error)
+        }
+        return requestObservable(method: method, with: data, at: endpoint, headers: headers, validation)
     }
 }
